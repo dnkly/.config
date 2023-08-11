3322832b5641ebcfc82dc2cd7ec32db6940b32e4
Den <138505681+dnkly@users.noreply.github.com>
2023-08-11 18:13:40 +0300

    modified fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set -U fish_greeting
 
 alias ll="exa -la"
 alias tree="exa --tree"
+alias bat="bat --theme gruvbox-dark"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
